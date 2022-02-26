4235810b247f090e2429c27b3f6cac80afb2be4d
emrhssla <emrhssla@gmail.com>
2022-02-26 15:45:33 +0900

    상품 목록 + 타임리프 전 HTML
    itms.html은 타임리프 적용전이다

---
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,45 @@
+package hello.itemservice.web.basic;
+
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+    private final ItemRepository itemRepository;
+
+    /*
+    @Autowired
+    public BasicItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+    ** @RequiredArgsConstructor이 이 역할
+     */
+
+    @GetMapping
+    public String items(Model model){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    @PostConstruct
+    public void init(){
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 10000, 10));
+    }
+
+
+}
